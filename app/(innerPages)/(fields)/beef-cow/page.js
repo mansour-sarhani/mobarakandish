b9841b183b4b1a685acd1f528704df9331680e44
mansour sarhani <mansour.sarhani@gmail.com>
2024-05-24 16:23:19 +0330

    Inner page update - beef cow

--- app/(innerPages)/(fields)/beef-cow/page.js
+++ app/(innerPages)/(fields)/beef-cow/page.js
@@ -1,3 +1,68 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Image from "next/image";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import FormatListNumberedRtlIcon from "@mui/icons-material/FormatListNumberedRtl";
+import Link from "next/link";
+
 export default function BeefCow() {
-	return <div style={{ height: "700px" }}>گاو گوشتی</div>;
+	return (
+		<Box className="service-block block-ltr">
+			<Grid container spacing={3}>
+				<Grid item xs={12} md={7}>
+					<Box className="service-visual">
+						<div className="service-overlay service-overlay-right"></div>
+						<Box className="service-main-image">
+							<Image
+								src="/images/sheep-service.webp"
+								width={900}
+								height={514}
+								alt="مبارک اندیش"
+							/>
+						</Box>
+					</Box>
+				</Grid>
+
+				<Grid item xs={12} md={5}>
+					<Box className="service-content">
+						<Typography
+							variant="h3"
+							gutterBottom
+							marginBottom="20px"
+						>
+							اصلاح نژاد گاو گوشتی
+						</Typography>
+						<Typography
+							variant="subtitle1"
+							color="primary"
+							gutterBottom
+							marginBottom="20px"
+						>
+							تولید و تامین مواد ژنتیکی ممتاز، با استفاده از دانش
+							روز و نوآوری ...
+						</Typography>
+						<Typography variant="body1" marginBottom="30px">
+							تولید کننده و تأمین کننده اسپرم و جنین نژادهای مختلف
+							گاوهای شیری و گوشتی، اسپرم و جنین نژادهای مختلف
+							گوسفند و بز، جوجه یکروزه مادر و پدر گوشتی، تخم چشم
+							زده و بچه ماهی قزل آلا و سالمون، مخازن و تانک های
+							حمل و نگهداری ازت و اکسیژن و نیز تجهیزات تلقیح
+							مصنوعی می باشد.
+						</Typography>
+
+						<div className="service-buttons-wrapper">
+							<Button
+								variant="contained"
+								color="primary"
+								startIcon={<FormatListNumberedRtlIcon />}
+							>
+								<Link href={"/"}>اسپرم گاو گوشتی</Link>
+							</Button>
+						</div>
+					</Box>
+				</Grid>
+			</Grid>
+		</Box>
+	);
 }
